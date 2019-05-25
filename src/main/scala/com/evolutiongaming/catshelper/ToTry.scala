9ee5259c7cf6f962b12e6b37cb9eb376b337d6e6
Yaroslav Klymko <t3hnar@gmail.com>
2019-05-25 22:20:16 +0200

    add ToTry

---
+++ src/main/scala/com/evolutiongaming/catshelper/ToTry.scala
@@ -0,0 +1,34 @@
+package com.evolutiongaming.catshelper
+
+import cats.effect.IO
+
+import scala.concurrent.TimeoutException
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+trait ToTry[F[_]] {
+
+  def apply[A](fa: F[A]): Try[A]
+}
+
+object ToTry {
+
+  def apply[F[_]](implicit F: ToTry[F]): ToTry[F] = F
+
+
+  def ioToTry(timeout: FiniteDuration): ToTry[IO] = new ToTry[IO] {
+
+    def apply[A](fa: IO[A]) = {
+
+      def error: Try[A] = Failure[A](new TimeoutException(timeout.toString()))
+
+      for {
+        a <- Try { fa.unsafeRunTimed(timeout) }
+        a <- a.fold(error) { a => Success(a) }
+      } yield a
+    }
+  }
+
+
+  implicit val ioToTry: ToTry[IO] = ioToTry(1.minute)
+}
---
+++ src/test/scala/com/evolutiongaming/catshelper/ToTrySpec.scala
@@ -0,0 +1,30 @@
+package com.evolutiongaming.catshelper
+
+import cats.effect.IO
+import cats.implicits._
+import com.evolutiongaming.catshelper.IOSuite._
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.util.{Failure, Success, Try}
+import scala.util.control.NoStackTrace
+
+class ToTrySpec extends FunSuite with Matchers {
+
+  def failure[A](a: Throwable): Try[A] = Failure(a)
+
+  def success[A](a: A): Try[A] = Success(a)
+
+  for {
+    (name, value, expected) <- List(
+      ("success", ().pure[IO],                success(())),
+      ("failure", Error.raiseError[IO, Unit], failure[Unit](Error))
+    )
+  } {
+    test(name) {
+      ToTry[IO].apply(value) shouldEqual expected
+    }
+  }
+
+
+  private case object Error extends RuntimeException with NoStackTrace
+}
