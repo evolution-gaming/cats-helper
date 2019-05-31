984b07d70cecd6f8b33e4b941c466d49bf1024bc
Yaroslav Klymko <t3hnar@gmail.com>
2019-05-31 20:22:41 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val scalatest   = "org.scalatest"     %% "scalatest" % "3.0.7"
-  val machinist   = "org.typelevel"     %% "machinist" % "0.6.6"
+  val machinist   = "org.typelevel"     %% "machinist" % "0.6.8"
   val `slf4j-api` = "org.slf4j"          % "slf4j-api" % "1.7.26"
   val `cats-par`  = "io.chrisdavenport" %% "cats-par"  % "0.2.1"
 
@@ -12,6 +12,6 @@ object Dependencies {
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.2.0"
+    val effect = "org.typelevel" %% "cats-effect" % "1.3.1"
   }
 }
