7506494c0efd5524e18589fafd4079d9637c6e4a
Yaroslav Klymko <t3hnar@gmail.com>
2019-08-24 21:43:45 +0200

    bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest   = "org.scalatest"     %% "scalatest" % "3.0.7"
+  val scalatest   = "org.scalatest"     %% "scalatest" % "3.0.8"
   val machinist   = "org.typelevel"     %% "machinist" % "0.6.8"
-  val `slf4j-api` = "org.slf4j"          % "slf4j-api" % "1.7.26"
+  val `slf4j-api` = "org.slf4j"          % "slf4j-api" % "1.7.28"
   val `cats-par`  = "io.chrisdavenport" %% "cats-par"  % "0.2.1"
 
   object Cats {
-    private val version = "1.6.0"
+    private val version = "1.6.1"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.3.1"
+    val effect = "org.typelevel" %% "cats-effect" % "1.4.0"
   }
 }
