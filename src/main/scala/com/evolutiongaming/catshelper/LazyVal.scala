0e6bd02ebfa1647bfa71c69d3558df8b76c312c0
Yaroslav Klymko <t3hnar@gmail.com>
2019-11-01 19:50:46 +0100

    improve ResourceFenced

--- src/main/scala/com/evolutiongaming/catshelper/LazyVal.scala
+++ src/main/scala/com/evolutiongaming/catshelper/LazyVal.scala
@@ -26,51 +26,53 @@ object LazyVal {
 
   def of[F[_] : Concurrent, A](load: => F[A]): F[LazyVal[F, A]] = {
     for {
-      ref <- Ref[F].of(none[Deferred[F, F[A]]])
+      ref <- Ref[F].of(none[F[A]])
     } yield {
       apply(ref, load)
     }
   }
 
   def apply[F[_] : Concurrent, A](
-    ref: Ref[F, Option[Deferred[F, F[A]]]],
+    ref: Ref[F, Option[F[A]]],
     load: => F[A]
   ): LazyVal[F, A] = {
     new LazyVal[F, A] {
 
       val get = {
 
-        def loaded = {
+        def loadOrJoin: F[A] = {
 
-          def complete(d: Deferred[F, F[A]]) = {
-            for {
-              a  <- load.attempt
-              af  = a.liftTo[F]
-              _  <- d.complete(af)
-            } yield af
+          def loadOrJoin(d: Deferred[F, Either[Throwable, A]]) = {
+            val load1 = for {
+              a <- load.attempt
+              _ <- d.complete(a)
+              a <- a.liftTo[F]
+            } yield a
+            ref
+              .modify {
+                case None    => (d.get.rethrow.some, load1)
+                case Some(a) => (a.some, a)
+              }
+              .flatten
+              .uncancelable
           }
 
           for {
-            d <- Deferred[F, F[A]]
-            a <- ref.modify {
-              case None    => (d.some, complete(d))
-              case Some(a) => (a.some, a.get)
-            }
-            a <- a
-          } yield a
+            loaded <- Deferred[F, Either[Throwable, A]]
+            result <- loadOrJoin(loaded)
+          } yield result
         }
 
         for {
           a <- ref.get
-          a <- a.fold(loaded.uncancelable)(_.get)
-          a <- a
+          a <- a.fold(loadOrJoin)(identity)
         } yield a
       }
 
       val getLoaded = {
         for {
           a <- ref.get
-          a <- a.fold(none[A].pure[F])(_.get.flatMap(_.map(_.some)))
+          a <- a.sequence
         } yield a
       }
     }
--- src/main/scala/com/evolutiongaming/catshelper/ResourceFenced.scala
+++ src/main/scala/com/evolutiongaming/catshelper/ResourceFenced.scala
@@ -3,7 +3,6 @@ package com.evolutiongaming.catshelper
 /**
  * Ensures release called at most once no matter what
  */
-import cats.effect.concurrent.{Deferred, Ref}
 import cats.effect.implicits._
 import cats.effect.{Concurrent, Resource}
 import cats.implicits._
@@ -13,32 +12,11 @@ object ResourceFenced {
   def apply[F[_] : Concurrent, A](resource: Resource[F, A]): Resource[F, A] = {
 
     val result = for {
-      ref <- Ref[F].of(none[F[Unit]])
-      ab  <- resource.allocated
+      ab           <- resource.allocated
+      (a, release)  = ab
+      released     <- LazyVal.of(release)
     } yield {
-      val (a, release) = ab
-
-      def releaseOrJoin(released: Deferred[F, Either[Throwable, Unit]]) = {
-        val release1 = for {
-          a <- release.attempt
-          _ <- released.complete(a)
-        } yield {}
-
-        ref
-          .modify {
-            case None    => (released.get.rethrow.some, release1)
-            case Some(a) => (a.some, a)
-          }
-          .flatten
-          .uncancelable
-      }
-
-      val release1 = for {
-        released <- Deferred[F, Either[Throwable, Unit]]
-        result   <- releaseOrJoin(released)
-      } yield result
-
-      (a, release1.uncancelable)
+      (a, released.get)
     }
 
     Resource(result.uncancelable)
