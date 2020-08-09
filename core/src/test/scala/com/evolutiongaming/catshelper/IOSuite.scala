f8853ed0b46dfe14eac4a3ed2308d9555f67b87c
Yaroslav Klymko <t3hnar@gmail.com>
2020-08-09 21:48:58 +0200

    fix compile error

--- core/src/test/scala/com/evolutiongaming/catshelper/IOSuite.scala
+++ core/src/test/scala/com/evolutiongaming/catshelper/IOSuite.scala
@@ -18,7 +18,7 @@ object IOSuite {
   implicit val parallel: Parallel[IO] = IO.ioParallel
 
   def runIO[A](io: IO[A], timeout: FiniteDuration = Timeout): Future[Succeeded.type] = {
-    io.timeout(timeout).as(Succeeded).unsafeToFuture
+    io.timeout(timeout).as(Succeeded).unsafeToFuture()
   }
 
   implicit class IOOps[A](val self: IO[A]) extends AnyVal {
