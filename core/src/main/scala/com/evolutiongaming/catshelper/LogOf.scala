225713ee26db5832ec801c184234910bcbc5b904
Anton Voitsishevskii <avoitsishevskii@evolution.com>
2021-11-16 11:08:34 +0200

    add cached Log and a simple log factory for it;

--- core/src/main/scala/com/evolutiongaming/catshelper/LogOf.scala
+++ core/src/main/scala/com/evolutiongaming/catshelper/LogOf.scala
@@ -2,8 +2,10 @@ package com.evolutiongaming.catshelper
 
 import cats.effect.Sync
 import cats.implicits._
-import cats.{Applicative, Functor, ~>}
+import cats.{Applicative, Functor, Id, ~>}
 import org.slf4j.{ILoggerFactory, LoggerFactory}
+
+import scala.reflect.ClassTag
 
 trait LogOf[F[_]] {
 
@@ -14,6 +16,25 @@ trait LogOf[F[_]] {
 
 object LogOf {
 
+
+  trait Safe[F[_]] {
+    def apply(source: String): Log[F]
+
+    def apply[Source: ClassTag]: Log[F]
+  }
+
+  def slf4jSafe[F[_] : Sync]: F[LogOf.Safe[F]] = {
+    for {
+      factory <- Sync[F].delay {
+        LoggerFactory.getILoggerFactory
+      }
+    } yield new Safe[F] {
+      override def apply(source: String): Log[F] = Log.cached(source, factory)
+
+      override def apply[Source: ClassTag]: Log[F] = Log.cached(implicitly[ClassTag[Source]].runtimeClass.getName.stripSuffix("$"), factory)
+    }
+  }
+
   def apply[F[_]](implicit F: LogOf[F]): LogOf[F] = F
 
   def summon[F[_]](implicit F: LogOf[F]): LogOf[F] = F
@@ -23,11 +44,14 @@ object LogOf {
 
     def apply(source: String) = {
       for {
-        log <- Sync[F].delay { factory.getLogger(source) }
+        log <- Sync[F].delay {
+          factory.getLogger(source)
+        }
       } yield {
         Log[F](log)
       }
     }
+
     def apply(source: Class[_]) = apply(source.getName.stripSuffix("$"))
   }
 
@@ -37,7 +61,9 @@ object LogOf {
 
   def slf4j[F[_] : Sync]: F[LogOf[F]] = {
     for {
-      factory <- Sync[F].delay { LoggerFactory.getILoggerFactory }
+      factory <- Sync[F].delay {
+        LoggerFactory.getILoggerFactory
+      }
     } yield {
       apply(factory)
     }
@@ -76,4 +102,6 @@ object LogOf {
       }
     }
   }
+
+
 }
