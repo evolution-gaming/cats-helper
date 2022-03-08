f1ef86c1a56f6249bf79850efa436a432dc78774
Yaroslav Klymko <t3hnar@gmail.com>
2022-03-08 11:45:18 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,20 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.10"
+  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.11"
   val machinist   = "org.typelevel"     %% "machinist"       % "0.6.8"
-  val `slf4j-api` = "org.slf4j"          % "slf4j-api"       % "1.7.32"
-  val logback     = "ch.qos.logback"     % "logback-classic" % "1.2.6"
+  val `slf4j-api` = "org.slf4j"          % "slf4j-api"       % "1.7.36"
+  val logback     = "ch.qos.logback"     % "logback-classic" % "1.2.11"
 
   val cpKindProjector = "org.typelevel" % "kind-projector" % "0.13.2" cross CrossVersion.full
 
   object Cats {
-    private val version = "2.6.1"
+    private val version = "2.7.0"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
 
-    private val effectVersion = "3.3.4"
+    private val effectVersion = "3.3.7"
     val effect     = "org.typelevel" %% "cats-effect"      % effectVersion
     val effectLaws = "org.typelevel" %% "cats-effect-laws" % effectVersion
     val effectTestkit = "org.typelevel" %% "cats-effect-testkit" % effectVersion
