7015ae7ebbccfecbad2eb9a355e8dd39cd86694b
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2022-05-17 11:32:03 +0200

    restore FromFuture.lift w. execution context param

--- core/src/main/scala/com/evolutiongaming/catshelper/FromFuture.scala
+++ core/src/main/scala/com/evolutiongaming/catshelper/FromFuture.scala
@@ -31,8 +31,29 @@ object FromFuture {
 
   def summon[F[_]](implicit F: FromFuture[F]): FromFuture[F] = F
 
+  implicit def lift[F[_]: Async](implicit executor: ExecutionContext): FromFuture[F] = {
 
-  implicit def lift[F[_] : Async]: FromFuture[F] = {
+    new FromFuture[F] {
+
+      def apply[A](future: => Future[A]) = {
+        for {
+          future <- Sync[F].delay(future)
+          result <- future.value.fold {
+            Async[F].async_[A] { callback =>
+              future.onComplete { a =>
+                callback(a.toEither)
+              }
+            }
+          } {
+            case Success(a) => Async[F].pure(a)
+            case Failure(a) => Async[F].raiseError[A](a)
+          }
+        } yield result
+      }
+    }
+  }
+
+  def asyncFromFuture[F[_]: Async]: FromFuture[F] = {
 
     new FromFuture[F] {
 
@@ -55,17 +76,15 @@ object FromFuture {
     }
   }
 
-
-  def functionK[F[_] : FromFuture]: FunctionK[Future, F] = new FunctionK[Future, F] {
+  def functionK[F[_]: FromFuture]: FunctionK[Future, F] =
+    new FunctionK[Future, F] {
 
     def apply[A](fa: Future[A]) = FromFuture.summon[F].apply(fa)
   }
 
-
   implicit val futureFromFuture: FromFuture[Future] = new FromFuture[Future] {
     def apply[A](future: => Future[A]) = future
   }
-
 
   implicit class FromFutureOps[F[_]](val self: FromFuture[F]) extends AnyVal {
 
