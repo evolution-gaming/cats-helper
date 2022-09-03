af29e24b63476528dd6f5262a892ed910ad6df4b
Scala Steward <scala_steward@virtuslab.com>
2022-09-03 13:34:15 +0000

    Update scalatest to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.11"
+  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.13"
   val `slf4j-api` = "org.slf4j"          % "slf4j-api"       % "1.7.36"
   val logback     = "ch.qos.logback"     % "logback-classic" % "1.2.11"
 
