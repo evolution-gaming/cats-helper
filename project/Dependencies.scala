927bcc508e492125278b70333b8f84582f368006
Andrei Mikhailov <amikhailov@evolution.com>
2022-09-19 12:40:17 +0200

    libs update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,17 +2,17 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.11"
-  val `slf4j-api` = "org.slf4j"          % "slf4j-api"       % "1.7.36"
-  val logback     = "ch.qos.logback"     % "logback-classic" % "1.2.11"
+  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.13"
+  val `slf4j-api` = "org.slf4j"          % "slf4j-api"       % "2.0.1"
+  val logback     = "ch.qos.logback"     % "logback-classic" % "1.4.1"
 
   object Cats {
-    private val version = "2.7.0"
+    private val version = "2.8.0"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
 
-    private val effectVersion = "3.3.7"
+    private val effectVersion = "3.3.14"
     val effect     = "org.typelevel" %% "cats-effect"      % effectVersion
     val effectLaws = "org.typelevel" %% "cats-effect-laws" % effectVersion
     val effectTestkit = "org.typelevel" %% "cats-effect-testkit" % effectVersion
