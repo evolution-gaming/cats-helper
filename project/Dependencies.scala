c29605fb5219bd88a6a92e5068bef169903ea9f5
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2023-01-06 13:02:57 +0100

    move logback based Log impl to separate module

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,23 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.11"
+  val scalatest   = "org.scalatest"     %% "scalatest"       % "3.2.14"
   val machinist   = "org.typelevel"     %% "machinist"       % "0.6.8"
-  val `slf4j-api` = "org.slf4j"          % "slf4j-api"       % "1.7.36"
-  val logback     = "ch.qos.logback"     % "logback-classic" % "1.2.11"
+  val `slf4j-api` = "org.slf4j"          % "slf4j-api"       % "2.0.5"
+
+  object Logback {
+    val classic = "ch.qos.logback" % "logback-classic" % "1.4.5"
+  }
 
   val cpKindProjector = "org.typelevel" % "kind-projector" % "0.13.2" cross CrossVersion.full
 
   object Cats {
-    private val version = "2.7.0"
+    private val version = "2.9.0"
     val core   = "org.typelevel" %% "cats-core"   % version
     val kernel = "org.typelevel" %% "cats-kernel" % version
     val macros = "org.typelevel" %% "cats-macros" % version
 
-    private val effectVersion = "2.5.4"
+    private val effectVersion = "2.5.5"
     val effect     = "org.typelevel" %% "cats-effect"      % effectVersion
     val effectLaws = "org.typelevel" %% "cats-effect-laws" % effectVersion
   }
