8fddfc93ec3a0fe5a4fade2ba10e184cf1139b89
Georgi Jojgov <gjojgov@evolution.com>
2023-06-14 09:59:48 +0200

    Move code to CatsHelper

--- core/src/main/scala/com/evolutiongaming/catshelper/CatsHelper.scala
+++ core/src/main/scala/com/evolutiongaming/catshelper/CatsHelper.scala
@@ -1,10 +1,10 @@
 package com.evolutiongaming.catshelper
 
 import cats.data.EitherT
-import cats.effect.implicits._
+import cats.effect.implicits.*
 import cats.effect.kernel.{Deferred, Fiber}
-import cats.effect.{Concurrent, Resource}
-import cats.implicits._
+import cats.effect.{Concurrent, Resource, Sync}
+import cats.implicits.*
 import cats.{Applicative, ApplicativeError, MonadError}
 
 import scala.concurrent.Future
@@ -143,6 +143,47 @@ object CatsHelper {
     def apply[A](left: => A)(implicit F: Applicative[F]): EitherT[F, A, Unit] = {
       val either = if (b) Left(left) else Right(())
       EitherT(either.pure[F])
+    }
+  }
+
+  implicit final class ResourceLogOps[F[_], A](val self: Resource[F, A]) extends AnyVal {
+    def log(name: String)(implicit F: Sync[F], log: Log[F], md: MeasureDuration[F]): Resource[F, A] = Resource {
+      for {
+        timedAcquireAndRelease <- for {
+          getMeasurement <- MeasureDuration[F].start
+          _ <- Log[F].info(s"$name acquiring")
+          a <- self.allocated.attemptTap {
+            case Left(err) => for {
+              measureResult <- getMeasurement
+              _ <- Log[F].error(s"$name acquisition failed in ${measureResult.toMillis}ms with $err", err)
+            } yield ()
+
+            case Right(_) => for {
+              measureResult <- getMeasurement
+              _ <- Log[F].info(s"$name acquired in ${measureResult.toMillis}ms")
+            } yield ()
+          }
+        } yield a
+
+        (timedAcquire, release) = timedAcquireAndRelease
+
+        timedRelease = for {
+          getMeasurement <- MeasureDuration[F].start
+          _ <- Log[F].info(s"$name releasing")
+          _ <- release.attemptTap {
+            case Left(err) => for {
+              measureResult <- getMeasurement
+              _ <- Log[F].error(s"$name release failed in ${measureResult.toMillis}ms with $err", err)
+            } yield ()
+
+            case Right(a) => for {
+              measureResult <- getMeasurement
+              _ <- Log[F].info(s"$name released in ${measureResult.toMillis}ms")
+            } yield a
+          }
+        } yield ()
+
+      } yield (timedAcquire, timedRelease)
     }
   }
 }
