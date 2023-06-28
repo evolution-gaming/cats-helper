3017528e257df36e2d68fb959ec415e420213d30
Kamil Podsiadlo <kamilpodsiadlo44@gmail.com>
2023-06-28 11:35:41 +0200

    fix indent

--- testkit/src/test/scala/com/evolutiongaming/catshelper/testkit/PureTestSpec.scala
+++ testkit/src/test/scala/com/evolutiongaming/catshelper/testkit/PureTestSpec.scala
@@ -10,42 +10,42 @@ import org.scalatest.matchers.should.Matchers._
 import scala.concurrent.duration._
 
 class PureTestSpec extends AnyFreeSpec {
-   "time flows as expected" in ioTest { env =>
-     import env._
-     for {
-       _ <- IO.race(IO.sleep(1.hour), IO.sleep(1.day))
-       _ <- testRuntime.getTimeSinceStart.map(_ shouldBe 1.hour)
-     } yield ()
-   }
+  "time flows as expected" in ioTest { env =>
+    import env._
+    for {
+      _ <- IO.race(IO.sleep(1.hour), IO.sleep(1.day))
+      _ <- testRuntime.getTimeSinceStart.map(_ shouldBe 1.hour)
+    } yield ()
+  }
 
-   "false assertion fails a test" in {
-     assertThrows[TestFailedException] {
-       ioTest { _ => IO { 1 shouldBe 2 } }
-     }
-   }
+  "false assertion fails a test" in {
+    assertThrows[TestFailedException] {
+      ioTest { _ => IO { 1 shouldBe 2 } }
+    }
+  }
 
-   "non-termination fails a test" in {
-     assertThrows[AbnormalTermination] {
-       ioTest { _ => IO.never }
-     }
-   }
+  "non-termination fails a test" in {
+    assertThrows[AbnormalTermination] {
+      ioTest { _ => IO.never }
+    }
+  }
 
-   "hot loop fails a test" in {
-     assertThrows[AbnormalTermination] {
-       PureTest.hotLoopTimeout(100.millis).ioTest { _ => IO.unit.foreverM }
-     }
-   }
+  "hot loop fails a test" in {
+    assertThrows[AbnormalTermination] {
+      PureTest.hotLoopTimeout(100.millis).ioTest { _ => IO.unit.foreverM }
+    }
+  }
 
-   "flakiness check" in {
-     // Usually flakiness comes from undefined concurrent execution order.
-     // Here we simulate it with a shared mutable state.
-     val counter = Ref.unsafe[IO, Int](0)
-     assertThrows[TestFailedException] {
-       PureTest.flakinessCheckIterations(5).ioTest { _ =>
-         counter.modify(i => (i + 1) -> i).map(_ should not be (4))
-       }
-     }
-   }
+  "flakiness check" in {
+    // Usually flakiness comes from undefined concurrent execution order.
+    // Here we simulate it with a shared mutable state.
+    val counter = Ref.unsafe[IO, Int](0)
+    assertThrows[TestFailedException] {
+      PureTest.flakinessCheckIterations(5).ioTest { _ =>
+        counter.modify(i => (i + 1) -> i).map(_ should not be (4))
+      }
+    }
+  }
 
   "is unaffected by infinite background loops" in ioTest { _ =>
     // "Unaffected" in this test case means that an infinite background loop does not
