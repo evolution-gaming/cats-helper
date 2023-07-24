d69f49beab7c3f6b4bf78a4ade1fcd161443f294
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2023-07-24 17:13:43 +0200

    implement ResourceCounter unit-tests

--- core/src/main/scala/com/evolutiongaming/catshelper/ResourceCounter.scala
+++ core/src/main/scala/com/evolutiongaming/catshelper/ResourceCounter.scala
@@ -3,6 +3,27 @@ package com.evolutiongaming.catshelper
 import cats.effect.{Concurrent, Resource}
 import cats.syntax.all.*
 
+/** [[ResourceCounter]] is a data structure that allows reusing one [[Resource]] multiple times avoiding re-allocation.
+  *
+  * Resource allocation is lazy and happened on allocation of [[ResourceCounter.resource]]. If [[ResourceCounter.resource]]
+  * allocated again - then the source resource will not be allocated and cached value will be used.
+  * In case of concurrent allocation of [[ResourceCounter.resource]] the source resource guaranteed to be allocated only once,
+  * same rule applied for releasing resource. The release will happened after last [[ResourceCounter.resource]] is released.
+  *
+  * [[ResourceCounter]] can be reused multiple times - after is was allocated/released first time one can allocate it again, sample:
+  * {{{
+  *   val source: Resource[IO, Unit] = ???
+  *   for {
+  *     rc <- ResourceCounter.of(source)
+  *     _ <- rc.resource.use_ // allocated & released
+  *     _ <- rc.resource.use_ // allocated & released again
+  *     _ <- rc.resource.use_ // allocated & released and again
+  *   } yield {}
+  * }}}
+  *
+  * @tparam F the effect type
+  * @tparam A the type of resource
+  */
 trait ResourceCounter[F[_], A] {
   def resource: Resource[F, A]
 }
