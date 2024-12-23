b166b52072532856f5c1952e5708abd4a297f9be
Scala Steward <scala_steward@virtuslab.com>
2024-12-23 16:35:30 +0000

    Update logback-classic to 1.5.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.16"
 
   object Logback {
-    val classic = "ch.qos.logback" % "logback-classic" % "1.5.12"
+    val classic = "ch.qos.logback" % "logback-classic" % "1.5.15"
   }
 
   object Cats {
