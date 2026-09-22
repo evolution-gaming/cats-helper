cf8a0e5e260d694552a69a20b934d501da12a5ed
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-22 18:14:35 +0200

    Update slf4j-api to 2.0.20 (#427)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.20"
-  val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.19"
+  val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.20"
 
   object Logback {
     val classic = "ch.qos.logback" % "logback-classic" % "1.6.3"
